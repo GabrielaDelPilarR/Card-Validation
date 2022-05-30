170fa8400d2811079bd004854e7e64d7bbf04d60
Gabriela Rojas <gabrieladelpilarrojas21@gmail.com>
2022-05-30 09:02:48 -0500

    a Proyecto terminado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+//Nombre de formulario
+//const formulario = document.getElementById('formCard');
+
+//Nombre del input
+//const cardNumbers = document.getElementById ('cardNumber')
+
+//Resultado 
+const resultCard = document.getElementById('result');
+const resultName = document.getElementById('result2')
+
+const buttonValidate= document.getElementById('validate');
+
+
+
+/*formulario.addEventListener("submit", (event)=>{
+    handleSubmitCard(event,formulario,resultCard)
+});
+*/
+/*let x;
+
+cardNumbers.addEventListener('input', (e)=>{
+
+    x = cardNumbers.value;
+    
+    const number = validator.maskify(e.target.value);
+    console.log(number)
+    if(number !==""){
+        cardNumbers.value = number.toString();
+
+    }
+
+});
+*/
+
+buttonValidate.addEventListener('click', (e) =>{
+    e.preventDefault();
+    
+    const cardNumb = document.getElementById('cardNumber').value 
+    
+    const isValid =  validator.isValid(cardNumb)
+    const name = document.getElementById("clientName").value
+
+    if(isValid === true){
+        resultCard.textContent = 'Tu tarjeta ' + validator.maskify(cardNumb) + ' es VALIDA' 
+        resultName.textContent =  'Gracias ' + `${name} `.toUpperCase()      
+    }else{        
+        resultCard.textContent = 'Tu tarjeta ' + validator.maskify(cardNumb) + ' es INVALIDA' 
+        resultName.textContent =  'Gracias ' + `${name} `.toUpperCase()       
+    }
+    
+}); 
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,42 @@
 const validator = {
-  // ...
-};
-
-export default validator;
+    // el objeto validador tiene dos propiedades isValid y maskify
+  
+    isValid:(cardNumber)=>{
+      //convertir en array y revertir
+      let arr = cardNumber.split('').reverse()
+      let suma = 0;
+      let n = 0;
+    for(let i=0; i<arr.length; i++){
+      if(i%2 === 1){
+        n= Number(arr[i])*2;
+        //sumar los dos digitos si excede a 9 
+        if(n>=10){
+          n= (n-10)+1
+        }
+    } else{
+      n= Number(arr[i])
+    }
+    suma = suma + n
+    } //retornar si la suma es divisible de 10 
+    return suma % 10 === 0 ? true : false
+    },
+  
+       
+  
+   maskify : (cardNumber)=>{
+      let arr = cardNumber.split('')
+      let string = ''
+      const recorrido= (cardNumber.length) -4;
+      for(let i=0; i < arr.length; i++){
+        if(i < recorrido){
+          string += '#'
+        }else{
+          string = string + arr[i]
+        }
+      } 
+      
+      return string
+    }
+  };
+    
+  export default validator;
